1091b57f0b24a96098d6bedc80e66fb05ae3ff83
gaaiuss <caio.gui.castro@gmail.com>
2026-04-09 15:39:42 -0300

    add: working cart

--- src/product/views.py
+++ src/product/views.py
@@ -1,10 +1,13 @@
 from typing import Any
 
+from django.contrib import messages
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse
 from django.views import View
 from django.views.generic import DetailView, ListView
 
-from product.models import Product
+from product.models import Product, Variation
 
 PER_PAGE = 9
 
@@ -25,7 +28,73 @@ class ProductDetail(DetailView):
 
 class AddToCart(View):
     def get(self, *args: Any, **kwargs: dict[str, Any]) -> HttpResponse:  # noqa: ANN401
-        return HttpResponse("AddToCart")
+        http_referer = self.request.META.get("HTTP_REFERER", reverse("product:list"))
+        variation_id = self.request.GET.get("vid")
+
+        if not variation_id:
+            messages.error(self.request, "Product does not exists")
+            return redirect(http_referer)
+
+        variation = get_object_or_404(Variation, id=variation_id)
+        variation_stock = variation.stock
+        product = variation.product
+
+        product_id = product.id  # type: ignore
+        product_name = product.name
+        variation_name = variation.name or ""
+        unit_price = variation.price
+        promo_unit_price = variation.promo_price
+        slug = product.slug
+        image = product.image.name if product.image else ""
+
+        if variation.stock < 1:
+            messages.error(self.request, "Insufficient stock")
+            return redirect(http_referer)
+
+        if not self.request.session.get("cart"):
+            self.request.session["cart"] = {}
+            self.request.session.save()
+
+        cart = self.request.session["cart"]
+
+        if variation_id in cart:
+            cart_amount = cart[variation_id]["amount"]
+            cart_amount += 1
+
+            if variation_stock < cart_amount:
+                messages.warning(
+                    self.request,
+                    f"Insufficient stock for {cart_amount}x for the product "
+                    f"'{product_name}'. We added {variation_stock}x in your cart",
+                )
+                cart_amount = variation_stock
+
+            cart[variation_id]["amount"] = cart_amount
+            cart[variation_id]["amount_price"] = unit_price * cart_amount
+            cart[variation_id]["amount_promo_price"] = promo_unit_price * cart_amount
+        else:
+            cart[variation_id] = {
+                "product_id": product_id,
+                "product_name": product_name,
+                "variation_name": variation_name,
+                "variation_id": variation_id,
+                "unit_price": unit_price,
+                "promo_unit_price": promo_unit_price,
+                "amount_price": unit_price,
+                "amount_promo_price": promo_unit_price,
+                "amount": 1,
+                "slug": slug,
+                "image": image,
+            }
+
+        self.request.session.save()
+
+        messages.success(
+            self.request,
+            f"{product_name} ({cart[variation_id]['amount']}x) added successfully!",
+        )
+
+        return redirect(http_referer)
 
 
 class RemoveFromCart(View):
